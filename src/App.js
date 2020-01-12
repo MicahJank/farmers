c63bfaa6df4ec5757647c83771803685b7277ad3
MicahJank <MicahJank@users.noreply.github.com>
2020-01-11 20:21:56 -0600

    added more components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 import Register from './components/Register.js';
+import Login from './components/Login.js';
+import Farmers from './components/Farmers.js';
 
 function App() {
   return (
     <div className="App">
       <Register />
       <Switch>
-        <Route path={'/login'} />
-        <Route path={'/farmers'} />
+        <Route path={'/login'} component={Login} />
+        <Route path={'/farmers'} component={Farmers} />
       </Switch>
     </div>
   );
---
+++ src/components/Farmers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Farmers = () => {
+
+    return (
+        <div>
+            Farmers Component
+        </div>
+    )
+}
+
+export default Farmers;
