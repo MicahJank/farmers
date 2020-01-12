f784ebc0367ea08246f9e5fe7272ac9619dc80f7
MicahJank <MicahJank@users.noreply.github.com>
2020-01-11 20:19:55 -0600

    setup

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Login = () => {
+
+    return (
+        <form>
+            <input />
+            <input />
+            <button type='submit'>Login</button>
+        </form>
+    )
+}
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Register = () => {
+
+    return (
+        <form>
+            <input />
+            <input />
+            <input />
+            <button type='submit'>Register</button>
+        </form>
+    )
+}
+
+export default Register;
